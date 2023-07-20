e11febef641b8530079e58b21bca4d4b0b11683e
Fernando Tzoc <fernandotzoc4@gmail.com>
2023-07-19 18:36:39 -0600

    Update

--- src/app/[locale]/components/Navbar.tsx
+++ src/app/[locale]/components/Navbar.tsx
@@ -16,18 +16,18 @@ export const Navbar = (props: Props) => {
   const { curLocale, titles, paths } = props
 
   return (
-    <nav className='sticky top-0 z-50 bg-customBGColor flex flex-wrap items-center justify-between p-6'>
-      <div className='flex gap-2 items-center flex-shrink-0 text-fontColorTitles mr-6'>
+    <nav className='sticky top-0 z-50 flex flex-wrap items-center justify-between bg-customBGColor p-6'>
+      <div className='mr-6 flex flex-shrink-0 items-center gap-2 text-fontColorTitles'>
         <Image src={logo} alt='logo icon' />
-        <span className='font-semibold text-xl tracking-tight'>Fernando</span>
+        <span className='text-xl font-semibold tracking-tight'>Fernando</span>
       </div>
       <div className='block lg:hidden'>
         <button
-          className='flex items-center px-3 py-2 border rounded text-gray-500 border-borderColor hover:text-white hover:border-white'
+          className='flex items-center rounded border border-borderColor px-3 py-2 text-gray-500 hover:border-white hover:text-white'
           onClick={() => setIsOpen(!isOpen)}
         >
           <svg
-            className='fill-current h-3 w-3'
+            className='h-3 w-3 fill-current'
             viewBox='0 0 20 20'
             xmlns='http://www.w3.org/2000/svg'
           >
@@ -39,14 +39,14 @@ export const Navbar = (props: Props) => {
       <div
         className={`w-full ${
           isOpen ? '' : 'hidden'
-        } lg:flex lg:items-center lg:w-auto`}
+        } lg:flex lg:w-auto lg:items-center`}
       >
         <div className='text-sm lg:flex-grow'>
           {titles.map((title, index) => {
             return (
               <Link
                 href={`/${paths[index]}`}
-                className={`text-fontColor hover:text-white block mt-4 lg:inline-block lg:mt-0 mr-4 hashtag ${
+                className={`hashtag mt-4 mr-4 block text-fontColor hover:text-white lg:mt-0 lg:inline-block ${
                   pathname === `/${paths[index]}` ? 'text-white' : ''
                 }`}
                 key={index}
@@ -58,7 +58,7 @@ export const Navbar = (props: Props) => {
           <Link
             href={`${pathname}`}
             locale='en'
-            className={`text-fontColor hover:text-white block mt-4 lg:inline-block lg:mt-0 mr-4 ${
+            className={`mt-4 mr-4 block text-fontColor hover:text-white lg:mt-0 lg:inline-block ${
               curLocale === 'en' ? 'text-white' : ''
             }`}
           >
@@ -67,7 +67,7 @@ export const Navbar = (props: Props) => {
           <Link
             href={`${pathname}`}
             locale='es'
-            className={`text-fontColor hover:text-white block mt-4 lg:inline-block lg:mt-0 ${
+            className={`mt-4 block text-fontColor hover:text-white lg:mt-0 lg:inline-block ${
               curLocale === 'es' ? 'text-white' : ''
             }`}
           >
