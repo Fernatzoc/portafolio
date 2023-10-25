8f643aef95b0fe022f085a94b80e09e907e2d9a3
Fernando Tzoc <fernandotzoc4@gmail.com>
2023-10-25 13:52:09 -0600

    Update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,4 @@ const withNextIntl = require('next-intl/plugin')(
   './src/i18n.ts'
 )
 
-module.exports = withNextIntl({
-  // Other Next.js configuration ...
-  experimental: { appDir: true }
-})
+module.exports = withNextIntl({})
